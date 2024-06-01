7d6212f1b13ba3af35ed8d5a0560f5b2ef0ec782
Kerry Buckley <kerryjbuckley@gmail.com>
2024-06-01 14:27:40 +0100

    Relax Elixir version in mixfile

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BlockingBuffer.MixProject do
     [
       app: :blocking_buffer,
       version: "0.1.0",
-      elixir: "~> 1.17-rc",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
